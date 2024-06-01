3b90b23d5d3450947dec2d3fe460a6101d97a953
Daniel Cabrera <dan.cabreraa@duocuc.cl>
2024-06-01 11:55:32 -0400

    Avances

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
 
   ngOnInit() {
+  }
+
+  //Metodo para ir a la pagina home (registro) desde TABS
+  GoHome(){
+    this.router.navigate(['/home']);
   }
 
 }
