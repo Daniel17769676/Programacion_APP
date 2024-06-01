dbf051d52fb3f55ed50dad95a1a25a9a3726fd7a
Daniel Cabrera <dan.cabreraa@duocuc.cl>
2024-06-01 18:55:23 -0400

    funcional hasta tab3 completar interpolacion

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +7,18 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./tab2.page.scss'],
 })
 export class Tab2Page implements OnInit {
+
+ //Variables que van a recibir los datos del crear rutina
+  nombreRutina: string = "";
+  tipoRutina: string = "";
+  duracionRutina: any = 0;
+  diasRutina: any = 0;
+  ejerciciosRutina: string = "";
+  repeticionesRutina: number = 0;
+  seriesRutina: number = 0;
+  descansoRutina: any = 0;
+  observacionesRutina: string = "";
+
 
   constructor(private router: Router, private activaterouter: ActivatedRoute) { }
 
@@ -16,9 +28,28 @@ export class Tab2Page implements OnInit {
 
  //Funcion para boton crear rutina
   anadirRutina(){
+    this.enviarDatosTabs3();
     this.router.navigate(['/tabs/tab3']);
     }
 
+
+  //Definimos la funcion para enviar los datos a tab3
+ enviarDatosTabs3() {
+    let navigationExtras: NavigationExtras = {
+      state: {
+        envia_tab2_nombreRutina: this.nombreRutina,
+        envia_tab2_tipoRutina: this.tipoRutina,
+        envia_tab2_duracionRutina: this.duracionRutina,
+        envia_tab2_diasRutina: this.diasRutina,
+        envia_tab2_ejerciciosRutina: this.ejerciciosRutina,
+        envia_tab2_repeticionesRutina: this.repeticionesRutina,
+        envia_tab2_seriesRutina: this.seriesRutina,
+        envia_tab2_descansoRutina: this.descansoRutina,
+        envia_tab2_observacionesRutina: this.observacionesRutina
+        }
+    }
+    this.router.navigate(['/tabs/tab3'], navigationExtras);//Con esta linea de codigo se envian los datos a la pagina tab1
+  }
 
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -7,7 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab3Page implements OnInit {
 
-  constructor() { }
+  //Definimos variables que van a recibir los datos del crear rutina
+  recibe_tab2_nombreRutina: string = "";
+  recibe_tab2_tipoRutina: string = "";
+  recibe_tab2_duracionRutina: any = 0;
+  recibe_tab2_diasRutina: any = 0;
+  recibe_tab2_ejerciciosRutina: string = "";
+  recibe_tab2_repeticionesRutina: number = 0;
+  recibe_tab2_seriesRutina: number = 0;
+  recibe_tab2_descansoRutina: any = 0;
+  recibe_tab2_observacionesRutina: string = "";
+
+  constructor(private router: Router, private activaterouter: ActivatedRoute) {
+    this.activaterouter.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation()?.extras?.state) {
+        this.recibe_tab2_nombreRutina = this.router.getCurrentNavigation()?.extras?.state?.['envia_tab2_nombreRutina'];
+        this.recibe_tab2_tipoRutina = this.router.getCurrentNavigation()?.extras?.state?.['envia_tab2_tipoRutina'];
+        this.recibe_tab2_duracionRutina = this.router.getCurrentNavigation()?.extras?.state?.['envia_tab2_duracionRutina'];
+        this.recibe_tab2_diasRutina = this.router.getCurrentNavigation()?.extras?.state?.['envia_tab2_diasRutina'];
+        this.recibe_tab2_ejerciciosRutina = this.router.getCurrentNavigation()?.extras?.state?.['envia_tab2_ejerciciosRutina'];
+        this.recibe_tab2_repeticionesRutina = this.router.getCurrentNavigation()?.extras?.state?.['envia_tab2_repeticionesRutina'];
+        this.recibe_tab2_seriesRutina = this.router.getCurrentNavigation()?.extras?.state?.['envia_tab2_seriesRutina'];
+        this.recibe_tab2_descansoRutina = this.router.getCurrentNavigation()?.extras?.state?.['envia_tab2_descansoRutina'];
+        this.recibe_tab2_observacionesRutina = this.router.getCurrentNavigation()?.extras?.state?.['envia_tab2_observacionesRutina'];
+        console.log();
+      }
+    }
+    )
+  }
 
   ngOnInit() {
   }
